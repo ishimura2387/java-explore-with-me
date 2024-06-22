0bca9c3156eda431ae8445310839b14cb3e6431e
Shestakov_Denis <shestakov_denis_job@mail.ru>
2024-06-22 22:26:49 +0800

    error docker

--- stats-service/client/src/main/java/ru/practicum/ewm/client/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/ewm/client/StatsClient.java
@@ -6,6 +6,7 @@ import ru.practicum.ewm.dto.EndpointHitDto;
 import ru.practicum.ewm.dto.ViewStatsDto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class StatsClient {
@@ -25,12 +26,22 @@ public class StatsClient {
                 .block();
     }
 
-    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
-        return webClient
+    public List<ViewStatsDto> getStats(LocalDateTime startLocalDateTime, LocalDateTime endLocalDateTime, List<String> uris, boolean unique) {
+        String start = startLocalDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String end = endLocalDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String urisString = "";
+        for (String uri : uris) {
+            urisString = urisString + "," + uri;
+        }
+        urisString = urisString.substring(1);
+        System.out.println(urisString);
+        List<ViewStatsDto> list = webClient
                 .get()
-                .uri("/stats?start={start}&end={end}&uris={uris}&unique={unique}", start, end, uris, unique)
+                .uri("/stats?start={start}&end={end}&uris={urisString}&unique={unique}", start, end, urisString, String.valueOf(unique))
                 .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(ViewStatsDto.class))
                 .collectList()
                 .block();
+        return list;
     }
 }
+
