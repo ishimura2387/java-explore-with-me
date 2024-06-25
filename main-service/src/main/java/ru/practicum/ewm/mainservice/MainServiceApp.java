e31c3ce4e3886d6aff8bade47ed07bcf8bec6d21
Shestakov_Denis <shestakov_denis_job@mail.ru>
2024-06-26 00:28:19 +0800

    for check v.2.0

--- main-service/src/main/java/ru/practicum/ewm/mainservice/MainServiceApp.java
+++ main-service/src/main/java/ru/practicum/ewm/mainservice/MainServiceApp.java
@@ -1,10 +1,7 @@
 package ru.practicum.ewm.mainservice;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import ru.practicum.ewm.client.StatsClient;
 import ru.practicum.ewm.dto.EndpointHitDto;
